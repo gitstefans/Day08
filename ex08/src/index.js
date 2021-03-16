d3a277f95b0e7a5f3b1c8656cafe21dda0c425c8
Stefan Skočo <stefansgit@outlook.com>
2021-03-16 10:14:08 +0100

    Argument with a Returned Value

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+// Only change code bellow this line
+var total = 0;
+
+function funcWithArg(digit) {
+  var digit = (digit + 9) / 5;
+  return digit;
+}
+
+total = funcWithArg(6);
+// Only change code above this line
+console.log(total);
+module.exports =  funcWithArg;
