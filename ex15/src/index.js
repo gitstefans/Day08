d155a71b84000cff0412b4c5701a5f2a09934783
Stefan Skočo <stefansgit@outlook.com>
2021-03-16 10:38:03 +0100

    Radix

---
+++ ex15/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+
+function convertToInt(str){
+  return parsed = parseInt(str, 16);
+}
+convertToInt("BA");
+console.log(convertToInt("BA"));
+console.log(convertToInt("F1"));
+console.log(convertToInt("JeffBezos"));
+
+// Only change code below this line
+module.exports = convertToInt;
