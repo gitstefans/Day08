6862f4630fbe22dc1fa5e9a5642959e870ce93ce
Stefan Skočo <stefansgit@outlook.com>
2021-03-16 10:26:52 +0100

    Generate Random Whole Numbers

---
+++ ex12/src/index.js
@@ -0,0 +1,10 @@
+// Only change code bellow this line
+
+function randomInteger() {
+  var result = Math.floor(Math.random() * 30);
+  return result;
+}
+randomInteger();
+console.log(randomInteger());
+// Only change code above this line
+module.exports = randomInteger;
