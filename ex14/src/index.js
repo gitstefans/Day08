7ebd20cf11357936156de81930708137d9471d6c
Stefan Skočo <stefansgit@outlook.com>
2021-03-16 10:35:01 +0100

    the parseInt

---
+++ ex14/src/index.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+
+function convertStrToInt(str) {
+  var parsedInt = parseInt(str);
+  return parsedInt;
+}
+convertStrToInt("44");
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+// Only change code above this line
+module.exports = convertStrToInt;
